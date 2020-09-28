4209c807ea1da04bfe45bac932b355b403cc943f
Jimmy Bisenius <me@jimmybweb.com>
2020-09-28 12:45:14 -0400

    Add button & container components

--- .nuxt/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Button } from '../../components/Button.vue'
-export { default as Container } from '../../components/Container.vue'
-export { default as Logo } from '../../components/Logo.vue'
-
-export const LazyButton = import('../../components/Button.vue' /* webpackChunkName: "components/Button" */).then(c => c.default || c)
-export const LazyContainer = import('../../components/Container.vue' /* webpackChunkName: "components/Container" */).then(c => c.default || c)
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
